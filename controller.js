1f7b74ad7343678a436311bfe4da42d2cd308b0f
teguh01 <teguh01@mhs.unsyiah.ac.id>
2020-11-14 00:54:59 +0700

    berhasil tampil semua data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi Berjalan")
+    response.ok("Aplikasi Berjalan",res)
+};
+
+// menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
 };
