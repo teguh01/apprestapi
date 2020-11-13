ce7e8bc589e1c0e8f13567d72023d49d76ff5784
teguh01 <teguh01@mhs.unsyiah.ac.id>
2020-11-14 02:24:28 +0700

    berhasil tambah data

--- controller.js
+++ controller.js
@@ -3,13 +3,13 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req,res){
-    response.ok("Aplikasi Berjalan",res)
+exports.index = function (req, res) {
+    response.ok("Aplikasi Berjalan", res)
 };
 
 // menampilkan semua data mahasiswa
-exports.tampilsemuamahasiswa = function(req,res){
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
         if(error){
             console.log(error);
         }else{
@@ -19,10 +19,10 @@ exports.tampilsemuamahasiswa = function(req,res){
 };
 
 // menampilkan semua data berdasarkan id
-exports.tampilberdasarkanid = function(req,res){
+exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
     connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
-        function(error, rows, fields){
+        function (error, rows, fields) {
             if(error){
                 console.log(error);
             }else{
@@ -31,3 +31,21 @@ exports.tampilberdasarkanid = function(req,res){
         }
     );
 };
+
+// menampilkan data mahasiswa
+exports.tambahMahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)',
+        [nim, nama, jurusan],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok("Berhasil Tambah Data", res)
+            }
+        }
+    );
+}
