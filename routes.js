89e5dbc607ffd654e6ee34f0c58551b7db90bf02
teguh01 <teguh01@mhs.unsyiah.ac.id>
2020-11-14 02:53:44 +0700

    berhasil hapus data

--- routes.js
+++ routes.js
@@ -16,5 +16,8 @@ module.exports = function(app){
         .post(jsonku.tambahMahasiswa);
 
     app.route('/ubah')
-        .put(jsonku.ubahMahasiswa)
+        .put(jsonku.ubahMahasiswa);
+
+    app.route('/hapus')
+        .delete(jsonku.hapusMahasiswa);
 }
